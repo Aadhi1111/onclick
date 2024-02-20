f7aeee76c63901db60db0f9f8150a05cc976df60
Aadhi1111 <94366686+Aadhi1111@users.noreply.github.com>
2024-02-21 01:02:29 +0530

    script.js

--- script.js
+++ script.js
@@ -19,13 +19,65 @@
 // ele.append(ele1);
 // document.body.append(ele);
 
+var labelfirst = document.createElement("label");
+labelfirst.setAttribute("for","firstname");
+labelfirst.innerHTML = "First Name";
+
+var firstinput = document.createElement("input");
+firstinput.setAttribute("id","firstname");
+firstinput.setAttribute("type","text");
+
+var break1 = document.createElement("br");
+var break2 = document.createElement("br");
+var break3 = document.createElement("br");
+var break4 = document.createElement("br");
+var break5 = document.createElement("br");
+var break6 = document.createElement("br");
+var break7 = document.createElement("br");
+var break8 = document.createElement("br");
+
+
+var labelmiddle = document.createElement("label");
+labelmiddle.setAttribute("for","middlename");
+labelmiddle.innerHTML="Middle Name";
+
+var middleinput = document.createElement("input");
+middleinput.setAttribute("id","middlename");
+middleinput.setAttribute("type","text");
+
+var labellast=document.createElement("label");
+labellast.setAttribute("for","lastname");
+labellast.innerHTML="Last Name";
+
+var lastinput = document.createElement("input");
+lastinput.setAttribute("id","lastname");
+lastinput.setAttribute("type","text");
+
+var labelmail = document.createElement("label");
+labelmail.setAttribute("for","e-mail");
+labelmail.innerHTML = "E-Mail";
+
+var mailinput = document.createElement("input");
+mailinput.setAttribute("id","e-mail");
+mailinput.setAttribute("type","email");
+
+var button = document.createElement("button");
+button.setAttribute("onclick","foo()");
+button.innerHTML = "click me";
+
+
+document.body.append(labelfirst,break1,firstinput,break2,labelmiddle,break3,middleinput,break4,labellast,break5,lastinput,break6,labelmail,break7,mailinput,break8,button);
+
 function foo(){
-    var fn=document.getElementById("firstname").value;
-    var mn=document.getElementById("middlename").value;
-    var ln=document.getElementById("lastname").value;
-    var em=document.getElementById("email").value;
-    console.log(fn);
-    console.log(mn);
-    console.log(ln);
-    console.log(em);
+    var first = document.getElementById("firstname").value;
+    console.log(`First Name:${first}`);
+    
+    var middle = document.getElementById("middlename").value;
+    console.log(`Middle Name:${middle}`);
+
+    var last = document.getElementById("lastname").value;
+    console.log(`Last Name:${last}`);
+
+    var mail = document.getElementById("e-mail").value;
+    console.log(`E-Mail:${mail}`);
 }
